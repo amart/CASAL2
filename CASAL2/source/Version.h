ba69e10759956049b713a1dc90bf0d89b9f53f0c
Samik Datta <samik.datta@niwa.co.nz>
2019-02-07 17:56:35 +1300

    Added Hollings unit test (not functioning yet).

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION d82ed2ec463a6a3cfa0b5950e8f82a6cf1fe12d1
-#define SOURCE_CONTROL_DATE "2019-02-06"
+#define SOURCE_CONTROL_REVISION dd64bd5accbfa5b4f3b01a9a27fc5f5d056929b5
+#define SOURCE_CONTROL_DATE "2019-02-07"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "February"
-#define SOURCE_CONTROL_TIME "19:52:01"
-#define SOURCE_CONTROL_VERSION "2019-02-06 19:52:01 UTC (rev. d82ed2ec)"
+#define SOURCE_CONTROL_TIME "01:50:56"
+#define SOURCE_CONTROL_VERSION "2019-02-07 01:50:56 UTC (rev. dd64bd5a)"
 #endif
