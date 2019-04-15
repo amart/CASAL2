254bbdb5f8a13d950741a8403d4063432863ab50
Teresa Amar <zta@uw.edu>
2019-04-15 13:40:30 +1200

    Switch modelrunner from betadiff to cppad

--- BuildSystem/buildtools/classes/ModelRunner.py
+++ BuildSystem/buildtools/classes/ModelRunner.py
@@ -19,12 +19,12 @@ class ModelRunner:
     if Globals.operating_system_ == 'windows':
       binary_name += '.exe'
 
-    if not os.path.exists('bin/' + Globals.operating_system_ + '/release_betadiff/' + binary_name):
-      print 'Looking for bin/' + Globals.operating_system_ + '/release_betadiff/' + binary_name
+    if not os.path.exists('bin/' + Globals.operating_system_ + '/release_cppad/' + binary_name):
+      print 'Looking for bin/' + Globals.operating_system_ + '/release_cppad/' + binary_name
       print 'CASAL2 binary was not found. Can not continue'
-      print 'Please complete a release betadiff binary build before running the models'
+      print 'Please complete a release cppad binary build before running the models'
       return False
-  
+
 
     print ''
     success_count = 0
@@ -32,7 +32,7 @@ class ModelRunner:
     estimation_dir_list = {"Simple", "TwoSex", "SBW"}
     dash_i_dir_list = {"Complex_input","TwoSex_input","SingleSexTagByLength_input"}
     dir_list = os.listdir("../TestModels/")
-    cwd = os.path.normpath(os.getcwd())  
+    cwd = os.path.normpath(os.getcwd())
     # test -r functionality with full/different models
     for folder in dir_list:
       if folder in estimation_dir_list:
@@ -44,9 +44,9 @@ class ModelRunner:
       if folder.startswith("DO NOT"):
         continue
       os.chdir("../TestModels/" + folder)
-      
+
       start = time.time()
-      
+
       result = False;
       if os.system("casal2 -r --loglevel=trace> run.log 2>&1") != EX_OK:
         elapsed = time.time() - start
@@ -56,8 +56,8 @@ class ModelRunner:
         elapsed = time.time() - start
         print '[OK] - ' + folder + ' in ' + str(round(elapsed, 2)) + ' seconds'
         success_count += 1
-      os.chdir(cwd)   
-    # test -i functionality 
+      os.chdir(cwd)
+    # test -i functionality
     for folder in dash_i_dir_list:
       os.chdir("../TestModels/" + folder)
       if os.system("casal2 -r -i pars.out > run.log 2>&1") != EX_OK:
@@ -68,20 +68,20 @@ class ModelRunner:
         elapsed = time.time() - start
         print '[OK] - ' + folder + ' -i run in ' + str(round(elapsed, 2)) + ' seconds'
         success_count += 1
-      os.chdir(cwd) 
+      os.chdir(cwd)
     # test -e functionality
     for folder in estimation_dir_list:
       os.chdir("../TestModels/" + folder)
       if os.system("casal2 -e -g 20 > estimate.log 2>&1") != EX_OK:
         elapsed = time.time() - start
-        print '[FAILED] - ' + folder + ' betadiff estimation in ' + str(round(elapsed, 2)) + ' seconds'
+        print '[FAILED] - ' + folder + ' cppad estimation in ' + str(round(elapsed, 2)) + ' seconds'
         fail_count += 1
       else:
         elapsed = time.time() - start
-        print '[OK] - ' + folder + ' betadiff estimation in ' + str(round(elapsed, 2)) + ' seconds'
+        print '[OK] - ' + folder + ' cppad estimation in ' + str(round(elapsed, 2)) + ' seconds'
         success_count += 1
-      os.chdir(cwd) 
-      
+      os.chdir(cwd)
+
     print ''
     print 'Total Models: ' + str(success_count + fail_count)
     print 'Failed Models: ' + str(fail_count)
--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 3b479e41d7d0836efb159e88882e6aba9324c53f
-#define SOURCE_CONTROL_DATE "2019-04-11"
+#define SOURCE_CONTROL_REVISION 040ed5384c1f2acb30d2a56e24562d768bb15972
+#define SOURCE_CONTROL_DATE "2019-04-12"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "April"
-#define SOURCE_CONTROL_TIME "21:44:07"
-#define SOURCE_CONTROL_VERSION "2019-04-11 21:44:07 UTC (rev. 3b479e4)"
+#define SOURCE_CONTROL_TIME "01:00:23"
+#define SOURCE_CONTROL_VERSION "2019-04-12 01:00:23 UTC (rev. 040ed53)"
 #endif
