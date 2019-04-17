e57d150e84de15dfc24205cc1faeb4531cb8b329
Teresa Amar <zta@uw.edu>
2019-04-17 17:01:15 +1200

    Changes to TestModel configuration to be explicit about using CppAD

--- CASAL2/source/Version.h
+++ CASAL2/source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 0ce982afff4250d1f7750c1ec45dbcb2290dfc57
-#define SOURCE_CONTROL_DATE "2019-04-16"
+#define SOURCE_CONTROL_REVISION 3bf7f4e68061b5ed2c383059545a03957f05133c
+#define SOURCE_CONTROL_DATE "2019-04-17"
 #define SOURCE_CONTROL_YEAR "2019"
 #define SOURCE_CONTROL_MONTH "April"
-#define SOURCE_CONTROL_TIME "05:29:00"
-#define SOURCE_CONTROL_VERSION "2019-04-16 05:29:00 UTC (rev. 0ce982a)"
+#define SOURCE_CONTROL_TIME "03:44:43"
+#define SOURCE_CONTROL_VERSION "2019-04-17 03:44:43 UTC (rev. 3bf7f4e)"
 #endif
